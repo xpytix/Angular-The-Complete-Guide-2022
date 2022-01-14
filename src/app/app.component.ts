5cfe3258e2e7aedf181bfe985833a0722099c4c0
Szymon Pytel <s.pytel@wasko.pl>
2022-01-14 14:57:13 +0100

    add nav with event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  loadedFeatures ='recipe'
+  onNavigate(feature: string){
+    this.loadedFeatures = feature;
+  }
 }

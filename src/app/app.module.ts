0384dd9d75d68a41078a2b7f45b0fb765c993474
Szymon Pytel <s.pytel@wasko.pl>
2022-01-20 12:03:17 +0100

    Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA  } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { DropdownDirective } from './shared/dropdown.directives';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipeDetailComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    ShoppingEditComponent,
-    ShoppingListComponent,
-    RecipesComponent,
-    DropdownDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA,
-    NO_ERRORS_SCHEMA
-  ]
-
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
